68e0e3aab2425ce98c7c4b3cad6ce2a7653bf223
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-09-16 09:59:32 +0200

    more stuff
    stuff

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,15 +1,34 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
     public GameObject Container;
 
+    private bool isOpen = true;
+    private float position = 1;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            Container.SetActive(!Container.activeSelf);
+            //Container.SetActive(!Container.activeSelf);
+            isOpen = isOpen == false;
+            
         }
+        if(isOpen == true)
+        {
+            position += 2f*Time.deltaTime;
+            position = Mathf.Clamp(position, 0,1);
+        }
+        else
+        {
+            position -= 2f*Time.deltaTime;
+            position = Mathf.Clamp(position, 0,1);
+        }
+
+        Container.GetComponent<RectTransform>().anchoredPosition = new Vector2(-290*(1-position), 0);
+        //Debug.Log(position);
     }
 }
